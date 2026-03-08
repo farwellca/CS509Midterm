a426d5c4dd996976290d10219da8e54244b0208a
farwellca <cwfarwell@wpi.edu>
2026-03-08 18:48:56 +0000

    added admin menu

--- model/AdminModel.cs
+++ model/AdminModel.cs
@@ -26,7 +26,42 @@ public static class AdminModel
 
     public static void ShowAdminMenu()
     {
-        Console.WriteLine("You have logged in as an admin!");
+        Console.Clear();
+
+        bool done = false;
+
+        while (!done)
+        {
+            Console.WriteLine("\n1--Create New Account\n2--Delete Exicting Accont\n3--Update Account Information\n4--Search for Account\n5--Exit");
+
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Create Account");
+                    break;
+                case "2":
+                    Console.WriteLine("Delete Account");
+                    break;
+                case "3":
+                    Console.WriteLine("Update Account");
+                    break;
+                case "4":
+                    Console.WriteLine("Search Account");
+                    break;
+                case "5":
+                    Console.Clear();
+                    Console.WriteLine("Have a nice day.");
+                    done = true;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+
+            }
+        }
     }
 
 
